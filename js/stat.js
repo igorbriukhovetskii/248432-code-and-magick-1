ec1a45b84fc8b8bcc6ea18970301c259a0ac1024
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2017-11-22 17:28:13 +0300

    Определяет функцию renderStatistics

---
+++ js/stat.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Функция отрисовки статистики прохождения игры.
+ * @param {Object} ctx - канвас, на котором рисуется игра
+ * @param {Array} names - массив имён игроков
+ * @param {Array} times - массив времён прохождения игры
+ */
+window.renderStatistics = function (ctx, names, times) {
+
+};
