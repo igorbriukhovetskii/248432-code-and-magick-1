81d039d522004bee964751b749153b731edd6dde
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2017-11-22 18:24:10 +0300

    Добавляет отрисовку облака для статистики

--- js/stat.js
+++ js/stat.js
@@ -7,5 +7,35 @@
  * @param {Array} times - массив времён прохождения игры
  */
 window.renderStatistics = function (ctx, names, times) {
+  var cloudXcoordinate = 100;
+  var cloudYcoordinate = 10;
+  var cloudWidth = 420;
+  var cloudHeight = 270;
+  var cloudColor = 'rgba(255, 255, 255, 1)';
+  var shadowXcoordinate = cloudXcoordinate + 10;
+  var shadowYcoordinate = cloudYcoordinate + 10;
+  var shadowWidth = cloudWidth;
+  var shadowHeight = cloudHeight;
+  var shadowColor = 'rgba(0, 0, 0, 0.7)';
 
+  /**
+   * Рисование прямоугольника и заливка его заданным цветом
+   * @param {string} color - цвет заливки прямоугольника
+   * @param {number} x - координата верхнего левого угла прямоугольника
+   * @param {number} y - координата верхнего левого угла прямоугольника
+   * @param {number} width - ширина прямоугольника
+   * @param {number} height - высота прямоугольника
+   */
+  var drawRectangle = function (color, x, y, width, height) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+  };
+
+  // Рисование облака для статистики
+  var drawCloud = function () {
+    drawRectangle(shadowColor, shadowXcoordinate, shadowYcoordinate, shadowWidth, shadowHeight);
+    drawRectangle(cloudColor, cloudXcoordinate, cloudYcoordinate, cloudWidth, cloudHeight);
+  };
+
+  drawCloud();
 };
